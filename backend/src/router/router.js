b69fc6993d490d5ebfafe4534129df2f1c8740b5
nathy <nathalia.cericatto@gmail.com>
2024-06-25 18:54:33 -0300

    middleware ta com problema

--- backend/src/router/router.js
+++ backend/src/router/router.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const controllers = require('../controllers/controllers');
-const middlewares = require('../middleware/middlewares');
+// const middlewares = require('../middleware/middlewares');
 
 
-router.get('/users/isAdminID/:adminId' , controllers.getAllUsers);
-router.post('/register', middlewares.VerifyEmail, controllers.registerUser);
-router.delete('/users/isAdminID/:adminId/:id', controllers.deleteUser);
-router.post('/login', middlewares.VerifyLogin, controllers.loginUser);
+router.get('/users/' , controllers.getAllUsers);
+router.post('/register', controllers.registerUser);
+router.delete('/users/:id', controllers.deleteUser);
+router.post('/login', controllers.loginUser);
 router.put('/users/:id', controllers.uptadeUser);
 
 module.exports = router
